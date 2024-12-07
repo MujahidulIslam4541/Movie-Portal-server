ccd51670eb56c20d1111887914e051ba5c636a94
MujahidulIslam4541 <mirifat4541@gmail.com>
2024-12-07 13:26:59 +0600

    Data get, post methode added

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PROT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
-console.log(process.env.USER_NAME);
-
-const uri =
-  `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.oo75q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.oo75q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +24,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // const database = client.db("sample_mflix");
+    // const movies = database.collection("movies");
+
+    const MoviesConnection = client.db("Movies_Portal").collection("Movies");
+
+    app.post("/movies", async (req, res) => {
+      const AddMovies = req.body;
+      const result = await MoviesConnection.insertOne(AddMovies);
+      res.send(result);
+    });
+
+    app.get("/movies", async (req, res) => {
+      const cursor = MoviesConnection.find();
+      const result = await cursor.toArray(cursor);
+      res.send(result);
+    });
+
+    app.get("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const quarry = { _id: new ObjectId(id) };
+      const result = await MoviesConnection.findOne(quarry);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -35,7 +57,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
