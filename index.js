26d8adb2c47d831277f67e82a90d9de1f5ddac73
MujahidulIslam4541 <mirifat4541@gmail.com>
2024-12-08 21:16:14 +0600

    Remove .env file from Git tracking

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(express.json());
 app.use(cors());
 
-const uri = `mongodb+srv://Movie-portal:IsUr5dzSgwlB9v3G@cluster0.oo75q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.oo75q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
