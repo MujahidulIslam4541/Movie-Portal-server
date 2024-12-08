c5542e76179f50afa563167370f3339f79f2658e
MujahidulIslam4541 <mirifat4541@gmail.com>
2024-12-08 12:39:00 +0600

    converting error

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(express.json());
 app.use(cors());
 
-const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.oo75q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://Movie-portal:IsUr5dzSgwlB9v3G@cluster0.oo75q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // const database = client.db("sample_mflix");
     // const movies = database.collection("movies");
@@ -33,58 +33,67 @@ async function run() {
       .db("Movies_Portal")
       .collection("favorite_movie");
 
-    app.post("/favorite", async (req, res) => {
-      const favoriteMovies = req.body;
-      const result = await favoritesMovies.insertOne(favoriteMovies);
-      res.send(result);
-    });
+    // app.post("/favorite", async (req, res) => {
+    //   const favoriteMovies = req.body;
+    //   const result = await favoritesMovies.insertOne(favoriteMovies);
+    //   res.send(result);
+    // });
 
-    app.get("/favorite", async (req, res) => {
-      const cursor = favoritesMovies.find();
-      const result = await cursor.toArray(cursor);
-      res.send(result);
-    });
+    // app.get("/favorite", async (req, res) => {
+    //   const cursor = favoritesMovies.find();
+    //   const result = await cursor.toArray(cursor);
+    //   res.send(result);
+    // });
 
-    app.get("/favorite/:id", async (req, res) => {
+    app.get("/favorites/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const quarry = { _id: new ObjectId(id) };
-      const result = await favoritesMovies.findOne(quarry);
+      console.log(quarry);
+      const result = await favoritesMovies.find(quarry);
+      console.log(result);
       res.send(result);
     });
 
+    // app.delete("/favorite/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const quarry = { _id: new ObjectId(id) };
+    //   const result = await favoritesMovies.deleteOne(quarry);
+    //   res.send(result);
+    // });
 
 
 
-    app.post("/movies", async (req, res) => {
-      const AddMovies = req.body;
-      const result = await MoviesConnection.insertOne(AddMovies);
-      res.send(result);
-    });
 
-    app.get("/movies", async (req, res) => {
-      const cursor = MoviesConnection.find();
-      const result = await cursor.toArray(cursor);
-      res.send(result);
-    });
+    // app.post("/movies", async (req, res) => {
+    //   const AddMovies = req.body;
+    //   const result = await MoviesConnection.insertOne(AddMovies);
+    //   res.send(result);
+    // });
 
-    app.get("/movies/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id);
-      const quarry = { _id: new ObjectId(id) };
-      const result = await MoviesConnection.findOne(quarry);
-      res.send(result);
-    });
+    // app.get("/movies", async (req, res) => {
+    //   const cursor = MoviesConnection.find();
+    //   const result = await cursor.toArray(cursor);
+    //   res.send(result);
+    // });
 
-    app.delete("/movies/:id", async (req, res) => {
-      const id = req.params.id;
-      const quarry = { _id: new ObjectId(id) };
-      const result = await MoviesConnection.deleteOne(quarry);
-      res.send(result);
-    });
+    // app.get("/movies/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   console.log(id);
+    //   const quarry = { _id: new ObjectId(id) };
+    //   const result = await MoviesConnection.findOne(quarry);
+    //   res.send(result);
+    // });
+
+    // app.delete("/movies/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const quarry = { _id: new ObjectId(id) };
+    //   const result = await MoviesConnection.deleteOne(quarry);
+    //   res.send(result);
+    // });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
